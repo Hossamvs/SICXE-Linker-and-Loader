95fede3a6823fbe2158e4bbaf4427bc1d5c9587d
Zerb <hossamvs911@gmail.com>
2018-12-23 21:15:37 +0200

    Added some utility functions

--- Linker.h
+++ Linker.h
@@ -75,61 +75,102 @@ int readFile(vector<vector<string>> &code){
             code[i++].push_back(tempNoSpace);
             tempNoSpace.clear();
         }
-
+        inFile.close();
         return i;
     }
 
-void readESTAB(vector<vector<int>> &estab){
-//        std::ifstream inFile("ESTAB.txt");
-//        std::string temp;
-//        std::string tempNoSpace="";
-//        int i=0;
-//
-//        while (std::getline(inFile, temp)){
-//
-//            for(int j=0;j<temp.size();j++){
-//
-//                if(temp[j] != ' ' && temp[j] != '\t' && temp[j] != '\n' && temp[j] != '\0'){
-//
-//                    tempNoSpace+=temp[j];
-//
-//                }else{
-//                    code[i].push_back(tempNoSpace);
-//                    tempNoSpace.clear();
-//                }
-//
-//            }
-//
-//            code[i++].push_back(tempNoSpace);
-//            tempNoSpace.clear();
-//        }
+void readESTAB(vector<vector<string>> &estab){
+        std::ifstream inFile("ESTAB.txt");
+        std::string temp;
+        std::string tempNoSpace="";
+        int i=0;
+
+        while (std::getline(inFile, temp)){
+
+            for(int j=0;j<temp.size();j++){
+
+                if(temp[j] != ' ' && temp[j] != '\t' && temp[j] != '\n' && temp[j] != '\0'){
+
+                    tempNoSpace+=temp[j];
+
+                }else{
+                    estab[i].push_back(tempNoSpace);
+                    tempNoSpace.clear();
+                }
+
+            }
+
+            estab[i++].push_back(tempNoSpace);
+            tempNoSpace.clear();
+        }
 }
 
-void extractObjectCode(vector<string>&objectCode, vector<vector<string>> code,int lines,vector<string>&address){
+void extractObjectCode(vector<int>&objectCode, vector<vector<string>> code,int lines,vector<int>&address,vector<int>startAddresses){
     int progAddr=0;
     int temp=0;
+    int k = 0;
     for(int i=0; i<lines ; i++){
             if(code[i][0]=="H"){
                 progAddr=hexStringToDec(code[i][2]);
-                //objectCode.insert(pair<string,string>(code[i][0], intToHexString(location[j])));
+                progAddr=startAddresses[0+k];
+                k++;
                 cout<<progAddr<<endl;
             }else if (code[i][0]=="T"){
                     cout<<"found"<<endl;
-                    temp=hexStringToDec(code[i][1]); //get starting address of text record
+                    temp=hexStringToDec(code[i][1]) + progAddr; //get starting address of text record
                 for(int j=2 ; j<code[i].size() ; j++){
                     if(code[i][j].size()== 6){
                         //objectCode.insert(pair<int,int>(temp,hexStringToDec(code[i][j])));
-                        objectCode.push_back(code[i][j]);
-                        address.push_back(intToHexString(temp));
+                        objectCode.push_back(hexStringToDec(code[i][j]));
+                        address.push_back((temp));
                         temp+=3;
                     }else if(code[i][j].size()== 8){
                         //objectCode.insert(pair<int,int>(temp,hexStringToDec(code[i][j])));
-                        objectCode.push_back(code[i][j]);
-                        address.push_back(intToHexString(temp));
+                        objectCode.push_back(hexStringToDec(code[i][j]));
+                        address.push_back((temp));
                         temp+=4;
                     }
 
                 }
+            }
+
+        }
+}
+
+void getStartAddresses(vector<vector<string>> estab,vector<int>&startAddresses){
+
+    for(int i=0 ; i<estab.size() ; i++){
+        if(estab[i].size() == 3){
+            startAddresses.push_back(hexStringToDec(estab[i][1]));
+        }
+    }
+
+}
+
+void getESTABvalues(vector<vector<string>> estab,map<string,string>&estabValues){
+
+    for(int i=0 ; i<estab.size() ; i++){
+        if(estab[i].size() == 2){
+            estabValues.insert(pair<string,string>(estab[i][0],estab[i][1]));
+        }
+    }
+}
+
+void extractModification(vector<vector<string>> code,int lines,vector<int>&modifyAddress,vector<int>&modifyObjectCode,vector<int>startAddresses,map<string,string>estabValues){
+    int progAddr=0;
+    int temp=0;
+    int k = 0;
+    for(int i=0; i<lines ; i++){
+            if(code[i][0]=="H"){
+                progAddr=hexStringToDec(code[i][2]);
+                progAddr=startAddresses[0+k];
+                k++;
+            }else if (code[i][0]=="M"){
+                if(code[i][2] == "05"){
+                    modifyAddress.push_back(hexStringToDec(code[i][1]) - 1);
+                    modifyObjectCode.push_back(hexStringToDec(estabValues.find()->second));
+                }
+
             }
 
         }
--- LinkingLoader.cpp
+++ LinkingLoader.cpp
@@ -8,18 +8,30 @@ using namespace std;
 
 int main(){
     vector<vector<string>> code(1000);
-    vector<vector<int>> estab(500);
-    vector<string>objectCode;
-    vector<string>address;
+    vector<vector<string>> estab(500);
+    map<string,string>estabValues;
+    vector<int>objectCode;
+    vector<int>address;
+    vector<int>modifyAddress;
+    vector<int>modifyObjectCode;
+    vector<int>startAddresses;
 
     int lines = readFile(code);
-    extractObjectCode(objectCode,code,lines,address);
+    readESTAB(estab);
+    getStartAddresses(estab,startAddresses);
+    getESTABvalues(estab,estabValues);
+    extractObjectCode(objectCode,code,lines,address,startAddresses);
+    //extractModification(code,lines,modifyAddress,modifyObjectCode)
     //cout<<code[0][1][0]<<endl;
 
 /*Debugging */
 //    for(int i=0; i<code.size() ; i++)
 //        for(int j = 0 ; j< code[i].size() ; j++)
 //            cout<<code[i][j]<<endl;
+
+//    for(int i=0; i<estab.size() ; i++)
+//        for(int j = 0 ; j< estab[i].size() ; j++)
+//            cout<<estab[i][j]<<endl;
 
 //    for( std::map<int,int>::const_iterator it = objectCode.begin(); it != objectCode.end(); ++it )
 //    {
@@ -28,8 +40,17 @@ int main(){
 //      cout<<value<<"\t"<<key<<endl;
 //
 //    }
-    for(int i=0; i<address.size(); i++){
-        cout<<address[i]<<" |  "<<objectCode[i]<<endl;
-    }
+
+//    for( std::map<string,string>::const_iterator it = estabValues.begin(); it != estabValues.end(); ++it )
+//    {
+//      string value = it->first;
+//      string key = it->second;
+//      cout<<value<<"\t"<<key<<endl;
+//
+//    }
+
+//    for(int i=0; i<address.size(); i++){
+//        cout<<address[i]<<" |  "<<objectCode[i]<<endl;
+//    }
     return 0;
 }
